50f5942e728c31b3f2f881f14b565cde2957382d
Martin Gratereaux <mgratereaux@mdsoft.com.do>
2019-10-10 14:43:25 -0400

    Mejoras al leer los archivos, Poder filtrar los querys por tabla.

--- Extractordesentencias/Form1.Designer.cs
+++ Extractordesentencias/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
@@ -41,9 +44,10 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.label3 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -53,14 +57,18 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Location = new System.Drawing.Point(9, 10);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(800, 663);
+            this.tabControl1.Size = new System.Drawing.Size(600, 539);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.textBox4);
             this.tabPage1.Controls.Add(this.radioButton2);
             this.tabPage1.Controls.Add(this.radioButton1);
             this.tabPage1.Controls.Add(this.label3);
@@ -70,36 +78,75 @@
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 634);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(592, 513);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Entrada";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(166, 274);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "UPDATE";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(166, 252);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "INSERT";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 252);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sentencia:";
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(123, 303);
+            this.label2.Location = new System.Drawing.Point(92, 183);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Criterio";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(221, 303);
+            this.textBox3.Location = new System.Drawing.Point(166, 183);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(332, 22);
+            this.textBox3.Size = new System.Drawing.Size(250, 20);
             this.textBox3.TabIndex = 4;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(294, 451);
+            this.button2.Location = new System.Drawing.Point(220, 333);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(196, 27);
+            this.button2.Size = new System.Drawing.Size(147, 22);
             this.button2.TabIndex = 3;
             this.button2.Text = "Procesar";
             this.button2.UseVisualStyleBackColor = true;
@@ -107,9 +154,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(571, 263);
+            this.button1.Location = new System.Drawing.Point(428, 151);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 2;
             this.button1.Text = "Abrir";
             this.button1.UseVisualStyleBackColor = true;
@@ -118,9 +166,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(123, 263);
+            this.label1.Location = new System.Drawing.Point(92, 151);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.Size = new System.Drawing.Size(52, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Directorio";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -128,29 +177,33 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(221, 264);
+            this.textBox1.Location = new System.Drawing.Point(166, 151);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(332, 22);
+            this.textBox1.Size = new System.Drawing.Size(250, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button4);
             this.tabPage2.Controls.Add(this.button3);
             this.tabPage2.Controls.Add(this.textBox2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 634);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(592, 513);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Resultado";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(611, 605);
+            this.button3.Location = new System.Drawing.Point(457, 490);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(175, 23);
+            this.button3.Size = new System.Drawing.Size(131, 19);
             this.button3.TabIndex = 1;
             this.button3.Text = "Grabar en un archivo";
             this.button3.UseVisualStyleBackColor = true;
@@ -158,10 +211,11 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(6, 6);
+            this.textBox2.Location = new System.Drawing.Point(4, 5);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(780, 593);
+            this.textBox2.Size = new System.Drawing.Size(586, 483);
             this.textBox2.TabIndex = 0;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
@@ -169,44 +223,53 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // label3
+            // label4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(123, 351);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 17);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Sentencia:";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 214);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tabla";
             // 
-            // radioButton1
+            // textBox4
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(221, 351);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(79, 21);
-            this.radioButton1.TabIndex = 7;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "INSERT";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.textBox4.Location = new System.Drawing.Point(166, 214);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(250, 20);
+            this.textBox4.TabIndex = 9;
             // 
-            // radioButton2
+            // button4
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(221, 378);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 21);
-            this.radioButton2.TabIndex = 8;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "UPDATE";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.button4.Location = new System.Drawing.Point(4, 490);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 19);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Limpiar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(439, 218);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "*Case Sensitive";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(824, 687);
+            this.ClientSize = new System.Drawing.Size(618, 558);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
@@ -236,6 +299,10 @@
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Extractordesentencias/Form1.cs
+++ Extractordesentencias/Form1.cs
@@ -8,19 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
-using System.Diagnostics;
+using System.Web;
 
 namespace Extractordesentencias
 {
     public partial class Form1 : Form
     {
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -48,6 +44,13 @@ namespace Extractordesentencias
             string path1 = textBox1.Text;
             var allFiles = Directory.GetFiles(path1, "*.txt", SearchOption.AllDirectories);
 
+            string Tabla = " ";
+
+            if (!string.IsNullOrEmpty(textBox4.Text))
+            {
+                Tabla = " " + textBox4.Text;
+            }
+
             foreach (var t in allFiles)
             {
                 string x;
@@ -56,88 +59,115 @@ namespace Extractordesentencias
                 nombre = Path.GetFileNameWithoutExtension(t);
 
                 string[] archivo;
+                string criterio = textBox3.Text;
+
+                if (!string.IsNullOrEmpty(criterio))
+                {
+                    archivo = x.Split(criterio.ToCharArray());
+                }
+                else
+                {
+                    archivo = x.Split(';');
+                }
+
+                string[] Texto;
 
 
                 if (radioButton1.Checked)
                 {
-                    archivo = x.Split(';');
-
                     for (int y = 0; y < archivo.Length; y++)
                     {
+                        Texto = archivo[y].Split('"');
+                        string Resultado = Texto.Where(xs => xs.Replace('"', ' ').StartsWith("INSERT INTO" + Tabla)).FirstOrDefault();
 
-                        char[] letras = archivo[y].ToCharArray();
-
-                        for (int g = 0; g < letras.Length; g++)
+                        if (!string.IsNullOrEmpty(Resultado))
                         {
-                            string linea;
-                            linea = "";
-                            if (letras[g] == 'I' && letras[g] != null && letras[g + 1] == 'N' && letras[g + 1] != null && letras[g + 2] == 'S' && letras[g + 2] != null)
-                            {
-                                for (int i = g; i < letras.Length - 1; i++)
-                                {
-                                    linea = string.Concat(linea, letras[i]);
-
-                                }
-
-                            }
-                            if (linea != "")
-                            {
-                                if (textBox3.Text != "" && linea.Substring(12, textBox3.Text.Length + 1) == String.Concat(textBox3.Text, " "))
-                                {
-                                    textBox2.Text = String.Concat(textBox2.Text, linea, "\r\n", "\r\n");
-                                }
-                            }
-
+                            textBox2.Text = String.Concat(textBox2.Text, Resultado, "\r\n", "\r\n");
                         }
                     }
-                    tabPage2.Show();
 
+                    //for (int y = 0; y < archivo.Length; y++)
+                    //{
+
+                    //    char[] letras = archivo[y].ToCharArray();
+
+                    //    for (int g = 0; g < letras.Length; g++)
+                    //    {
+                    //        string linea;
+                    //        linea = "";
+                    //        if (letras[g] == 'I' && letras[g] != null && letras[g + 1] == 'N' && letras[g + 1] != null && letras[g + 2] == 'S' && letras[g + 2] != null)
+                    //        {
+                    //            for (int i = g; i < letras.Length - 1; i++)
+                    //            {
+                    //                linea = string.Concat(linea, letras[i]);
+
+                    //            }
+
+                    //        }
+                    //        if (linea != "")
+                    //        {
+                    //            if (textBox3.Text != "" && linea.Substring(12, textBox3.Text.Length + 1) == String.Concat(textBox3.Text, " "))
+                    //            {
+                    //                textBox2.Text = String.Concat(textBox2.Text, linea, "\r\n", "\r\n");
+                    //            }
+                    //        }
+
+                    //    }
+                    //}
                 }
                 else
                 {
                     if (radioButton2.Checked)
                     {
-                        archivo = x.Split('.');
-
                         for (int y = 0; y < archivo.Length; y++)
                         {
+                            Texto = archivo[y].Split('"');
+                            string Resultado = Texto.Where(xs => xs.Replace('"', ' ').StartsWith("UPDATE" + Tabla)).FirstOrDefault();
 
-                            char[] letras = archivo[y].ToCharArray();
-
-                            for (int g = 0; g < letras.Length; g++)
+                            if (!string.IsNullOrEmpty(Resultado))
                             {
-                                string linea;
-                                linea = "";
-                                if (letras[g] == 'U' && letras[g] != null && letras[g + 1] == 'P' && letras[g + 1] != null && letras[g + 2] == 'D' && letras[g + 2] != null)
-                                {
-                                    for (int i = g; i < letras.Length; i++)
-                                    {
-                                        linea = string.Concat(linea, letras[i]);
-
-                                    }
-
-                                }
-                                if (linea != "")
-                                {
-                                    if (textBox3.Text != "" && linea.Substring(12, textBox3.Text.Length + 1) == String.Concat(textBox3.Text, " "))
-                                    {
-                                        textBox2.Text = String.Concat(textBox2.Text, linea, "\r\n", "\r\n");
-                                    }
-                                    else
-                                    {
-                                        textBox2.Text = String.Concat(textBox2.Text, linea, "\r\n", "\r\n");
-                                    }
-                                }
-
+                                textBox2.Text = String.Concat(textBox2.Text, Resultado, "\r\n", "\r\n");
                             }
                         }
-                        tabPage2.Show();
+
+                        //for (int y = 0; y < archivo.Length; y++)
+                        //{
+
+                        //    char[] letras = archivo[y].ToCharArray();
+
+                        //    for (int g = 0; g < letras.Length; g++)
+                        //    {
+                        //        string linea;
+                        //        linea = "";
+                        //        if (letras[g] == 'U' && letras[g] != null && letras[g + 1] == 'P' && letras[g + 1] != null && letras[g + 2] == 'D' && letras[g + 2] != null)
+                        //        {
+                        //            for (int i = g; i < letras.Length; i++)
+                        //            {
+                        //                linea = string.Concat(linea, letras[i]);
+
+                        //            }
+
+                        //        }
+                        //        if (linea != "")
+                        //        {
+                        //            if (textBox3.Text != "" && linea.Substring(12, textBox3.Text.Length + 1) == String.Concat(textBox3.Text, " "))
+                        //            {
+                        //                textBox2.Text = String.Concat(textBox2.Text, linea, "\r\n", "\r\n");
+                        //            }
+                        //            else
+                        //            {
+                        //                textBox2.Text = String.Concat(textBox2.Text, linea, "\r\n", "\r\n");
+                        //            }
+                        //        }
+
+                        //    }
+                        //}
 
                     }
                 }
-
-
             }
+            MessageBox.Show("Datos procesados correctamente!", "Mensaje");
+            tabPage2.Show();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -147,19 +177,35 @@ namespace Extractordesentencias
 
         private void button3_Click_1(object sender, EventArgs e)
         {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Archivo txt (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.Title = "Guardar Archivo";
+            saveFileDialog1.ShowDialog();
+
+            string FileName = saveFileDialog1.FileName;
+
             StreamWriter nuevo;
-            if (textBox3.Text != "")
-            {
-                nuevo = new StreamWriter("Sentencias" + textBox3.Text + ".txt");
-            }
-            else
-            {
-                nuevo = new StreamWriter("Sentencias" + ".txt");
-            }
+            nuevo = new StreamWriter(FileName);
             nuevo.Write(textBox2.Text);
             nuevo.WriteLine();
-            nuevo.Close();
             MessageBox.Show("El archivo se almaceno correctamente", "Mensaje");
+            nuevo.Close();
+
+
+            //StreamWriter nuevo;
+            //if (textBox3.Text != "")
+            //{
+            //    nuevo = new StreamWriter("Sentencias" + textBox3.Text + ".txt");
+            //}
+            //else
+            //{
+            //    nuevo = new StreamWriter("Sentencias" + ".txt");
+            //}
+            //nuevo.Write(textBox2.Text);
+            //nuevo.WriteLine();
+            //nuevo.Close();
+            //MessageBox.Show("El archivo se almaceno correctamente", "Mensaje");
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -175,6 +221,11 @@ namespace Extractordesentencias
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
         }
     }
 }
